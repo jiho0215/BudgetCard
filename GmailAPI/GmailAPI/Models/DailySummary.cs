0a695582e6f8965991619180d1c76ef29529e18d
Lee, Jiho <jlee3841@gatech.edu>
2021-10-24 15:38:14 -0400

    refresh to up-to-date code
    refresh to up-to-date code

--- GmailAPI/GmailAPI/Models/DailySummary.cs
+++ GmailAPI/GmailAPI/Models/DailySummary.cs
@@ -8,12 +8,17 @@ namespace GmailAPI.Models
 {
     public class DailySummary
     {
+        public DailySummary()
+        {
+            WithdrawalList = new List<Transaction>();
+            DepositList = new List<Transaction>();
+        }
         public DateTime Date { get; set; }
-        public int EndingDigits { get; set; }
+        public string EndingDigits { get; set; }
         public float EndOfDayBalance { get; set; }
         public float TotalWithdrawls { get; set; }
         public List<Transaction> WithdrawalList { get; set; }
-        public float TotalDeposit { get; set; }
+        public float TotalDeposits { get; set; }
         public List<Transaction> DepositList { get; set; }
     }
 }
